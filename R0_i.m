31fc6ed8b2840f2a17858c5423c8c242eec2a56b
Duy Nguyen <hoanduy27@gmail.com>
2020-07-24 14:37:41 +0700

    Add R0_i for estimating R0 trend

--- R0_i.m
+++ R0_i.m
@@ -1,3 +1,5 @@
+%We will call the value that is proportional with E(R0), in (21), E*.
+%E* = Sigma(pi(X | beta_i, gamma_i)*pi(beta_i, gamma_i)).
 function E_R0s = R0_i(file, m)
     [I, R] = readFile(file);
     n = size(I,2) - 3;
@@ -6,35 +8,28 @@ function E_R0s = R0_i(file, m)
     E_R0s = zeros(weeks,1);
     E_R0_log = zeros(weeks, 1);
   
-    %Calculate E(R0) each weeek, start from March 6th in 'Singapore.csv'.
+    %Estimate E* in each weeek, start from March 6th in 'Singapore.csv'.
     for i = 1 : weeks
         E_R0s(i) = R_i(I, R, i, m);
         E_R0_log(i) = 1/abs(log(E_R0s(i)));
     end
-    figure(2)
     
+    %%%Plot%%%
+    figure(3)
     X1 = linspace(0, n, weeks + 1);
-    %{
-    Y1 = zeros(1, n);
-    for i = 2 : n
-        Y1(i) = (I(i) - I(i - 1) + 1)/(R(i) - R(i - 1) + 1);
-    end
-    %}
     Y1 = zeros(1, weeks + 1);
     for i = 2 : weeks + 1
         s = 7*(i - 2) + 1;
         e = 7*(i - 1) + 1;
-        %Y1(i) = mean((I(s + 1 : e) - I(s : e - 1) + 1) / (R(s + 1 : e) - R(s : e - 1) + 1));
-        Y1(i) = log((I(e) - I(s))) - log((R(e) - R(s)));
+        Y1(i) = mean((I(s + 1 : e) - I(s : e - 1) + 0.01) / (R(s + 1 : e) - R(s : e - 1) + 0.01));
+        %Y1(i) = (I(e) - I(s))/(R(e) - R(s));
     end
-    disp(Y1);
     Y1 = Y1/max(Y1);
     
     X2 = linspace(0, n, weeks + 1);
-    disp(X2);
     Y2 = zeros(1, weeks + 1);
     Y2(2:weeks + 1) = E_R0_log/max(E_R0_log);
-    title('Growth rate of I, R and E(R0)')
+    
     plot(X1,Y1);       
     hold on
     plot(X2, Y2, 'r');
@@ -42,14 +37,14 @@ function E_R0s = R0_i(file, m)
     legend('dI/dR', 'E(R0)');
 end
 
+%Estimate E* given the week.
 function E_R0 = R_i(I, R, week, m)
-    population = 5850343;
-    %population = 97338583;
+    population = 5850343; %Singapore population
     n = 7;
     %Calculate Sigma(pi(X|b_i, c_i)*b/c)
     numerator = 0;
     %Calculate Sigma(pi(X))
-    pi_X = 0;
+    %pi_X = 0;
     
     %Calculate start (s) date and last date (e) of the week
     s = n*(week - 1) + 1;
@@ -58,6 +53,7 @@ function E_R0 = R_i(I, R, week, m)
         e = size(I,2);
     end
     
+    %Estimate beta, gamma in each day
     beta_mat = zeros(s - e);
     gamma_mat = zeros(s - e);
     for i = s + 1 : e
@@ -70,26 +66,21 @@ function E_R0 = R_i(I, R, week, m)
     fprintf('R: %d -> %d\n', R(s), R(e));
     fprintf('Prior [b g]: %f %f with R0 = %f\n', mean(beta_mat), mean(gamma_mat), mean(beta_mat)/mean(gamma_mat));
     
-    a = 0.1;
-    b = 0.1;
-    
-    v_beta = mean(beta_mat)/var(beta_mat);
-    lambda_beta = v_beta*mean(beta_mat);
-    v_gamma = mean(gamma_mat)/var(gamma_mat);
-    lambda_gamma = v_gamma*mean(gamma_mat);
-    
+    %Sampling based on mean of beta_mat and gamma_mat.
+    %n = a for beta, = b for gamma.
+    a = mean(beta_mat)*0.01;
+    b = mean(gamma_mat)*0.01;
     sample = metropolis_hastings(mean(beta_mat)*a, mean(gamma_mat)*b, a, b, m);
-    %sample = metropolis_hastings(2, 2, 2/mean(beta_mat), 2/mean(gamma_mat), m);
     
     for i = 1 : size(sample, 1)
-        %pi
+        %Calculate pi(X|beta_i, gamma_i), X(t(i)) = I(i).
         pi = prod(gampdf(I(s:e), sample(i,1), 1/sample(i,2)));
         numerator = numerator + pi*(sample(i,1)/sample(i,2));
-        pi_X = pi_X + pi;
+        %pi_X = pi_X + pi;
     end
-    pi_X = 1;
-    E_R0 = numerator/pi_X;
-    fprintf('%f\n\n', log(E_R0));
+    %E_R0 = numerator/pi_X;
+    E_R0 = numerator;
+    fprintf('e^%f\n\n', log(E_R0));
     
 end
 
