4e27c5b1578c0a7f5ccacdee7b6c74405280b974
hoanduy27 <50733059+hoanduy27@users.noreply.github.com>
2020-07-24 19:38:09 +0700

    Update SIR_Euler.m

--- report/Code/baitoan2/SIR_Euler.m
+++ report/Code/baitoan2/SIR_Euler.m
@@ -1,35 +1,47 @@
-%TODO: Using Euler's algorithm to find solutions for SIR systems
-%INPUT:
-    % - t: vector contain time varaiable t
-    % - beta: infectious coefficient
-    % - gama: recovery coefficient
-    % - S0: value of S at time t0
-    % - I0: value of I at time t0
-    % - R0: value of R at time t0
-%OUTPUT: 
-    % - S_t: vector contain value of S at correspond time in vector t
-    % - I_t: vector contain value of I at correspond time in vector t
-    % - R_t: vector contain value of R at correspond time in vector t
-function [S_t,I_t,R_t]=SIR_Euler(t,beta,gama,S0,I0,R0)
+%% Description %%
+% TODO: Using Euler's algorithm to find solutions for SIR systems
+% INPUT:
+    % - t: time varaiable t
+    % - beta: contact coefficient
+    % - gamma: recovery coefficient
+    % - S0: S(t0)
+    % - I0: I(t0)
+    % - R0: R(t0)
+% OUTPUT: 
+    % - ret: vector [S(t) I(t) R(t)]
+function ret=SIR_Euler(t,beta,gamma,S0,I0,R0)
+    %% Initial %%
     syms diff_S(I,S) diff_I(I,S) diff_R(I)
-    
-    diff_S(I,S) = -beta*I*S; % S'(t)
-    diff_I(I,S) = beta*I*S - gama*I; % I'(t)
-    diff_R(I) = gama*I; % R'(t)
-  
-    S_t = []; S_t(1) = S0; % initial for S_t
-    I_t = []; I_t(1) = I0; % initial for I_t
-    R_t = []; R_t(1) = R0; % initial for R_t
-    
-    % Euler's algorithm
-    for i=2:length(t) 
+    N = S0+I0+R0; 
+    diff_S(I,S) = -beta/N*I*S; % S'(t)
+    diff_I(I,S) = beta/N*I*S - gamma*I; % I'(t)
+    diff_R(I) = gamma*I; % R'(t)
+    S_n = []; S_n(1) = S0; % initial for S_n series 
+    I_n = []; I_n(1) = I0; % initial for I_n series
+    R_n = []; R_n(1) = R0; % initial for R_n series
+
+    %% Euler's algorithm %%
+    step_size = 0.1 ; % We chose step size (delta t) = 0.1
+    t_n = [0:step_size:t]; % t_n series
+    for i=2:length(t_n) 
         % S_n+1 = S_n + delta_t*S'(t_n)
-        S_t(i) = S_t(i-1) + (t(i)-t(i-1))*diff_S(I_t(i-1),S_t(i-1)); 
-        
+        S_n(i) = S_n(i-1) + step_size*diff_S(I_n(i-1),S_n(i-1)); 
         % I_n+1 = I_n + delta_t*I'(t_n)
-        I_t(i) = I_t(i-1) + (t(i)-t(i-1))*diff_I(I_t(i-1),S_t(i-1)); 
-        
+        I_n(i) = I_n(i-1) + step_size*diff_I(I_n(i-1),S_n(i-1)); 
         % R_n+1 = R_n + delta_t*R'(t_n)
-        R_t(i) = R_t(i-1) + (t(i)-t(i-1))*diff_R(I_t(i-1)); 
+        R_n(i) = R_n(i-1) + step_size*diff_R(I_n(i-1)); 
     end 
+
+    %% Return result %%
+    ret=[];
+    ret(1) = S_n(length(S_n)); % ret(1)=S(t)
+    ret(2) = I_n(length(I_n)); % ret(2)=I(t)
+    ret(3) = R_n(length(R_n)); % ret(3)=R(t)
+
+    %% Draw Graph %%
+    plot(t_n,S_n,'-b',t_n,I_n,'-r',t_n,R_n,'-g')
+    legend('S(t)', 'I(t)', 'R(t)')
+    xlabel('Time')
+    ylabel('Number of people')
+    set(gca,'XTick',0:1:t)
 end     
